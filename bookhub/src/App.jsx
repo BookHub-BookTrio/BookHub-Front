fab3e1a391f3119fa9644077a66aba57047138d9
ttttkii913 <dbgkdud821@daum.net>
2025-04-07 18:22:00 +0900

    Refactor: app.jsx 다시 생성

---
+++ bookhub/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Test from "../src/pages/Test.jsx";
+import Home from "../src/pages/home/Home.jsx";
+import MyPage from "./pages/mypage/MyPage.jsx";
+import Login from "./pages/login/Login.jsx";
+import KakaoCallback from "./pages/home/KakaoCallback.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/mypage-edit" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth/kakao" element={<KakaoCallback />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
