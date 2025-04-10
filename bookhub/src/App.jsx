8a07c3ce083bf465332327d3140de87926fa1540
minjeong <0519suzy@naver.com>
2025-04-10 16:52:34 +0900

    [Feat/#12] - MyPage 초기 세팅

--- bookhub/src/App.jsx
+++ bookhub/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Test from "../src/pages/Test.jsx";
 import Home from "../src/pages/home/Home.jsx";
+import MyPage from "../src/pages/mypage/MyPage.jsx";
 import MyPageEdit from "../src/pages/mypage/MyPageEdit.jsx";
 import Login from "../src/pages/login/Login.jsx";
 import KakaoCallback from "./pages/home/KakaoCallback.jsx";
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/test" element={<Test />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
         <Route path="/mypage-edit" element={<MyPageEdit />} />
         <Route path="/login" element={<Login />} />
         <Route path="/auth/kakao" element={<KakaoCallback />} />
